53c517c7de0b959fd6d34628aa8937ad56c0c033
fredyyfajarr <fredyfajar46@gmail.com>
2025-02-04 11:38:18 +0700

    kontolll update lagi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
-import daisyui from "daisyui";
-
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        montserrat: ["Montserrat", "sans-serif"],
-      },
-    },
-  },
-  plugins: [daisyui],
-  daisyui: {
-    themes: ["winter"],
-=======
 import daisyui from 'daisyui';
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [daisyui],
   daisyui: {
     themes: ['winter'],
->>>>>>> b6020ed (first commit)
   },
 };
